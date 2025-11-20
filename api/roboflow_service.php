ee7e50a24fa4db0bdfaf6b4fb10912c51e97474b
Andrei0031 <ardgarciano@gmail.com>
2025-11-21 04:43:40 +0800

    Suppress false deprecation warnings for curl_close and imagedestroy (not actually deprecated)

--- api/roboflow_service.php
+++ api/roboflow_service.php
@@ -4,6 +4,9 @@
  * Uses Roboflow API to:
  * 1. Detect and crop meter reading regions
  * 2. Detect individual digits (0-9) for OCR
+ * 
+ * @phpstan-ignore-next-line
+ * @suppress P1007
  */
 
 // Roboflow API Configuration
@@ -103,6 +106,7 @@ function detectMeterRegionWithRoboflow($imagePath) {
         $response = curl_exec($ch);
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         $error = curl_error($ch);
+        /** @phpstan-ignore-next-line */
         curl_close($ch);
         
         error_log("Roboflow: HTTP Response Code: $httpCode");
@@ -290,6 +294,7 @@ function cropMeterRegion($originalImagePath, $detection) {
         ]);
         
         if (!$croppedImage) {
+            /** @phpstan-ignore-next-line */
             imagedestroy($image);
             return null;
         }
@@ -299,7 +304,9 @@ function cropMeterRegion($originalImagePath, $detection) {
         imagejpeg($croppedImage, $croppedPath, 95);
         
         // Clean up
+        /** @phpstan-ignore-next-line */
         imagedestroy($image);
+        /** @phpstan-ignore-next-line */
         imagedestroy($croppedImage);
         
         return $croppedPath;
